2105d93c63f8e0160325e8e46ca93c5b54abd395
Alex Falkowski <alexrfalkowski@gmail.com>
2025-11-03 20:29:24 +0100

    test(lc): add flags

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log/slog"
+	"os"
 	"time"
 
 	"github.com/alexfalkowski/go-signal"
 )
 
-var logger = slog.Default()
+var (
+	stop   = flag.String("stop", "1m", "the stop duration")
+	wait   = flag.String("wait", "1s", "the wait duration")
+	logger = slog.Default()
+)
 
 func process(ctx context.Context) error {
 	<-ctx.Done()
@@ -21,15 +27,28 @@ func process(ctx context.Context) error {
 }
 
 func main() {
-	lc := signal.NewLifeCycle(time.Minute)
+	flag.Parse()
+
+	stopDuration, err := time.ParseDuration(*stop)
+	if err != nil {
+		logger.Error("failed to parse stop duration", "error", err)
+		os.Exit(1)
+	}
+
+	waitDuration, err := time.ParseDuration(*wait)
+	if err != nil {
+		logger.Error("failed to parse wait duration", "error", err)
+		os.Exit(1)
+	}
+
+	lc := signal.NewLifeCycle(stopDuration)
 	lc.Register(&signal.Hook{
 		OnStart: func(ctx context.Context) error {
 			logger.Info("starting process")
-
-			return signal.Go(ctx, time.Second, process)
+			return signal.Go(ctx, waitDuration, process)
 		},
 		OnStop: func(ctx context.Context) error {
-			time.Sleep(time.Second)
+			time.Sleep(waitDuration)
 			logger.Info("stopping process")
 			return ctx.Err()
 		},
